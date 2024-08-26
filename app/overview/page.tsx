331b994aee027d9af086d810c80088bfb2740d69
Sarawut <sarawutu@scg.com>
2024-08-26 13:55:09 +0700

    install flowbite

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,19 +1,22 @@
 'use client'
 import { useSession } from "next-auth/react";
-
+import { Alert } from "flowbite-react";
+import { Button } from 'flowbite-react';
+import { HiInformationCircle } from "react-icons/hi";
 export default function OverviewPage() {
   const { data: session , status } = useSession();
 
-  console.log( session);
-  console.log(status);
-  
   
   return (
     status === 'authenticated' && session.user && (
     <div>
       <h1>Overview Page</h1>
       {session ? (
-        <p>Welcome, {session.user?.name}!</p>
+        <>
+          <p>Welcome, {session.user?.name}!</p>
+          <Alert color="failure" icon={HiInformationCircle}>Alert!</Alert>
+        </>
+        
       ) : (
         <p>You are not logged in</p>
       )}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,19 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite-react/lib/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
+    flowbite.content(),
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin"), flowbite.plugin()],
 };
+
 export default config;
